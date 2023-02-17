ed001afe66bb384731d79199c42bc64ca61cae55
JoshuaAv07 <a6520150001@utch.edu.mx>
2023-02-17 17:41:50 -0600

    1st commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodYParser = require('body-parser');
+const cors = require('cors');
+
+
+app.use(cors());
+app.listen()
+
+module.exports = app;
